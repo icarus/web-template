c5589ad8ab471abc1a1a5a2201d2fa2f26177f01
Felipe Mandiola <hi@icarusmind.com>
2024-12-28 23:15:50 -0300

    next-intl working

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextIntl = createNextIntlPlugin();
 
-export default nextConfig;
+export default withNextIntl({
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+  },
+  webpack(config) {
+    return config;
+  },
+  nextIntl: {
+    request: './app/i18n/request.ts',
+  },
+});
